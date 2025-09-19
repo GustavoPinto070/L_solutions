15ff05f741e6af53086633c22e94d56ffe62c53e
GustavoPinto070 <gustavogupinto@gmail.com>
2025-09-19 10:07:26 +0100

    typo oopsie

--- L05_MINITESTE(19-09)/ex3.py
+++ L05_MINITESTE(19-09)/ex3.py
@@ -1,4 +1,4 @@
-# Escreva um programa que leia um inteiro positivo n e calcule o produto dos seus dígitos ímpares.
+# Escreva um programa que leia um inteiro positivo n e calcule o produto dos seus divisores ímpares.
 
 n = int(input("Escreva um inteiro positivo: "))
 resultado = 1
@@ -7,4 +7,5 @@ while i <= n//2:                        # Testar todos os inteiros entre 2 e n/2
     if n % i == 0 and i % 2 == 1:       # i é divisor de n e é ímpar
         resultado = resultado * i
     i = i + 1                           # Pretty sure i forgot to increment i in the original code
-print("O produto dos dígitos ímpares é:", resultado)
+
+print("O produto dos divisores ímpares é:", resultado)
