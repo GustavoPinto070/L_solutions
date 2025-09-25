ce14fc0c6374dee353dfffb400d12b6e03522711
Gustavo Pinto <gustavogupinto@gmail.com>
2025-09-25 11:02:31 +0100

    documentation and explanation

--- L08/ex6.py
+++ L08/ex6.py
@@ -1,14 +1,22 @@
 def soma_mat(matriz1:list[list[int]], matriz2:list[list[int]])->list[list[int]]:
-    matriz_resultante = [[]]
-    if len(matriz1) != len(matriz2):
+    """
+    matriz1 = a = (m x n), 0 < i < m, 0 < j < n, cada elemento é dado por aij
+    matriz2 = b = (m x n), 0 < i < m, 0 < j < n, cada elemento é dado por bij
+    m = numero de linhas e "altura" da matriz
+    n = numero de colunas e "largura" da matriz
+    i = "altura" do elemento xij, com indice 0 no topo, crescendo para baixo
+    j = "largura" do elemento xij, com indice 0 na esquerda, crescendo para a direita
+    """
+    matriz_resultante = [[]]                                # m x n
+    if len(matriz1) != len(matriz2):                        # m1 != m2
         raise ValueError("soma_mat: matrizes com numero de linhas diferente")
-    for i in range(len(matriz1)):
-        linha = []
-        if len(matriz1[i]) != len(matriz2[i]):
+    for i in range(len(matriz1)):                           # por cada i in range(m)
+        linha = []                                          # len(linha) = n
+        if len(matriz1[i]) != len(matriz2[i]):              # n1 != n2
             raise ValueError("soma_mat: matrizes com numero de colunas diferente")
-        for j in range(len(matriz1[i])):
-            linha.append(matriz1[i][j] + matriz2[i][j])
-        matriz_resultante.append(linha)
+        for j in range(len(matriz1[i])):                    # por cada i in range(n)
+            linha.append(matriz1[i][j] + matriz2[i][j])     # cada elemento da linha vai ser a soma entre aij e bij
+        matriz_resultante.append(linha)                     # a matriz resultante é a lista de linhas (m x n)
     return matriz_resultante
 
 if __name__ == "__main__":
--- L08/ex7.py
+++ L08/ex7.py
@@ -1,15 +1,19 @@
 def multiplica_mat(matriz1:list[list[int]], matriz2:list[list[int]])->list[list[int]]:
+    """
+    matriz1 = a = (m x n), cada elemento é dado por aij
+    matriz2 = b = (n x r), cada elemento é dado por bij
+    """
     matriz_resultante = []
-    for m in range(len(matriz1)):
+    for m in range(len(matriz1)):                       # por cada linha da primeira matriz
         linha = []
-        for r in range(len(matriz2[0])):
+        for r in range(len(matriz2[0])):                # por cada coluna da segunda matriz
             if len(matriz1[0]) != len(matriz2):
-                raise ValueError("multiplica_mat: as matrizes não são m x n e n x r respetivamente")
-            soma = 0
-            for n in range(len(matriz2)):
-                soma += matriz1[m][n] * matriz2[n][r]
-            linha.append(soma)
-        matriz_resultante.append(linha)
+                raise ValueError("multiplica_mat: as matrizes não são m x n e n x r, respetivamente")
+            soma = 0                                    # cada elemento da matriz_resultante vai ser a soma da multiplicação
+            for n in range(len(matriz2)):               # por cada coluna da primeira/linha da segunda matriz
+                soma += matriz1[m][n] * matriz2[n][r]   # a parte da multiplicação entre elementos da linha m e coluna n, somados
+            linha.append(soma)                          # cada elemento é a soma das diferentes multiplicações (len(linha) = r)
+        matriz_resultante.append(linha)                 # a matriz resultante é a lista de linhas (m x r)
     return matriz_resultante
 
 if __name__ == "__main__":
