75b924c30075f9f482b720f4991ef0d8e9e4592e
Gustavo Pinto <gustavogupinto@gmail.com>
2025-09-26 11:32:50 +0100

    even more unreadable

--- L08/ex9.py
+++ L08/ex9.py
@@ -8,10 +8,10 @@ def num_occ_lista(lista:list[int|list], num:int)->int:
     return count
 
 # same thing but unreadable
-def num_occ_lista_pro(l,n): return sum(num_occ_lista_pro(x,n) if isinstance(x,list)else 1 if x==n else 0 for x in l)
+def f(l,n): return sum(f(x,n)if isinstance(x,list)else 1 if x==n else 0 for x in l)
 
 if __name__ == "__main__":
     test_list = [1, [[[1]], 2], [[[2]]], 2]
     print(r1:=(num_occ_lista(test_list, 2)))
-    print(r2:=(num_occ_lista_pro(test_list, 2)))
+    print(r2:=(f(test_list, 2)))
     print("Test ->", r1 == r2)
