b037ee941f4693c9eb4846da8078d4097bef1c61
Gustavo Pinto <gustavo.c.pinto@tecnico.ulisboa.pt>
2025-09-29 15:34:48 +0100

    fix

--- L08/ex6.py
+++ L08/ex6.py
@@ -7,7 +7,7 @@ def soma_mat(matriz1:list[list[int]], matriz2:list[list[int]])->list[list[int]]:
     i = "altura" do elemento xij, com indice 0 no topo, crescendo para baixo
     j = "largura" do elemento xij, com indice 0 na esquerda, crescendo para a direita
     """
-    matriz_resultante = [[]]                                # m x n
+    matriz_resultante = []                                # m x n
     if len(matriz1) != len(matriz2):                        # m1 != m2
         raise ValueError("soma_mat: matrizes com numero de linhas diferente")
     for i in range(len(matriz1)):                           # por cada i in range(m)
@@ -19,9 +19,14 @@ def soma_mat(matriz1:list[list[int]], matriz2:list[list[int]])->list[list[int]]:
         matriz_resultante.append(linha)                     # a matriz resultante é a lista de linhas (m x n)
     return matriz_resultante
 
+def soma_mat_resumo(m1, m2):
+    return [[m1[i][j] + m2[i][j] for j in range(len(m1[i]))] for i in range(len(m1))]
+
 if __name__ == "__main__":
     from ex5 import escreve_matriz
     m1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     m2 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-    escreve_matriz(soma_mat(m1, m2))
+    escreve_matriz(r1:=soma_mat(m1, m2))
+    escreve_matriz(r2:=soma_mat_resumo(m1, m2))
+    print(f"Test -> {r1 == r2}")
--- L08/ex9.py
+++ L08/ex9.py
@@ -7,6 +7,25 @@ def num_occ_lista(lista:list[int|list], num:int)->int:
             count += 1
     return count
 
+def num_occ_lista_alisa(lista, num):
+    def alisa(lista):
+        i = 0
+        while i < len(lista):
+            if isinstance(lista[i], list):
+                lista = lista[:i] + lista[i] + lista[i+1:]
+            else:
+                i += 1
+        return lista
+    
+    lista = alisa(lista)
+    count = 0
+
+    for elem in lista:
+        if elem == num:
+            count += 1
+    
+    return count
+
 if __name__ == "__main__":
     # same thing but unreadable
     def f(l,n): return sum(f(x,n)if isinstance(x,list)else 1 if x==n else 0 for x in l)
@@ -14,4 +33,5 @@ if __name__ == "__main__":
     test_list = [1, [[[1]], 2], [[[2]]], 2]
     print(r1:=(num_occ_lista(test_list, 2))) # 3
     print(r2:=(f(test_list, 2))) # 3
-    print("Test ->", r1 == r2) # True
+    print(r3:=(num_occ_lista_alisa(test_list, 2))) # 3 (this one changes the list directly, be careful)
+    print("Test ->", r1 == r2 == r3) # True
