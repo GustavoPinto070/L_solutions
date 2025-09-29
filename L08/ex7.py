61533be4f5bf2352d5854a9363c94c0ca81083cd
Gustavo Pinto <gustavo.c.pinto@tecnico.ulisboa.pt>
2025-09-29 14:42:09 +0100

    ex7 but better

--- L08/ex7.py
+++ L08/ex7.py
@@ -8,23 +8,29 @@ def multiplica_mat(matriz1:list[list[int]], matriz2:list[list[int]])->list[list[
         linha = []
         for r in range(len(matriz2[0])):                # por cada coluna da segunda matriz
             if len(matriz1[0]) != len(matriz2):
-                raise ValueError("multiplica_mat: as matrizes não são m x n e n x r, respetivamente")
+                raise ValueError("multiplica_mat: as matrizes não são m x n e n x r, respetivamente") # n1 != n2
             soma = 0                                    # cada elemento da matriz_resultante vai ser a soma da multiplicação
             for n in range(len(matriz2)):               # por cada coluna da primeira matriz
-          # for n in range(len(matriz1[0]))             # por cada linha da segunda matriz
+          # for n in range(len(matriz1[0]))             # por cada linha da segunda matriz (same bs)
                 soma += matriz1[m][n] * matriz2[n][r]   # a parte da multiplicação entre elementos da linha m e coluna r, somados
             linha.append(soma)                          # cada elemento é a soma das diferentes multiplicações (len(linha) = r)
         matriz_resultante.append(linha)                 # a matriz resultante é a lista de linhas (m x r)
     return matriz_resultante
+
+def multiplica_mat_resumo(m1, m2):
+    return [[sum(m1[m][n] * m2[n][r] for n in range(len(m2))) for r in range(len(m2[0]))] for m in range(len(m1))]
 
 if __name__ == "__main__":
     from ex5 import escreve_matriz
     m1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     m2 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-    escreve_matriz(multiplica_mat(m1, m2))
+    escreve_matriz(r1:=multiplica_mat(m1, m2))
+    escreve_matriz(r2:=multiplica_mat_resumo(m1, m2))
+    print(F"Test -> {r1 == r2}")
+
     """
-    30 36 42
-    66 81 96
-    102 126 150
+    30   36   42
+    66   81   96
+    102  126  150
     """
